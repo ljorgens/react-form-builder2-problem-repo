ae818c76e0f15c4cf0dad592cef99e86f8a5d61f
ljorgens <leejorgensen22@gmail.com>
2023-04-16 16:12:49 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { ReactFormBuilder } from 'react-form-builder2';
+import 'react-form-builder2/dist/app.css';
+
+function App() {
+  return (
+      <ReactFormBuilder />
+  );
+}
+
+export default App;
